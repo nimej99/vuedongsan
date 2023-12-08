b671ec492c4516ab56664add4b09a03b32b1cf49
nimej99 <aaron9909@naver.com>
2023-12-08 17:58:11 +0900

    [Feat] 데이터 바인딩, 이벤트리스너

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav class="menu">
+      <a href="" v-for="(a, i) in index" :key="i">{{ a }}</a>
+    </nav>
+
+    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <div v-for="(data, index) in 3" :key="index">
+      <h4>{{ products.title[index] }}</h4>
+      <p>{{ products.price[index] }}</p>
+      <button @click="increase(index)">허위매물신고</button>
+      <span>신고수 : {{ products.reportCount[index] }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  //데이터 보관함
+  data() {
+    return {
+      index: ["Home", "Products", "About"],
+      products: {
+        title: ["역삼동원룸", "마포구원룸", "천호동원룸"],
+        price: [50, 60, 70],
+        reportCount: [0, 0, 0],
+      },
+    };
+  },
+  methods: {
+    increase(index) {
+      this.products.reportCount[index]++;
+    },
+  },
+
+  components: {},
+};
 </script>
 
 <style>
@@ -21,6 +46,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
